b908f1401a3a3ada11f0b4820b5e795d84e770a5
SoumitraPratap <101451346+SoumitraPratap@users.noreply.github.com>
2023-10-24 11:11:12 +0530

    Updated news.js

--- src/MyComponents/news.js
+++ src/MyComponents/news.js
@@ -2,46 +2,40 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class news extends Component {
+    
+    constructor(){
+        super();
+        this.state={
+            articles: [],
+            loading:false
+            
+        }
+    }
+
+    async componentDidMount(){
+        let url="https://newsapi.org/v2/everything?q=india&from=2023-09-24&sortBy=publishedAt&apiKey=aaee3d61e4684322813323f585d68de7";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles:parsedData.articles});
+    }
     render() {
         return (
             <div className="container my-3">
                 <h2>News Central - Because you deserve nothing but the truth</h2>
+                
                 <br></br>
                 <div className="row">
 
-                    <div className="col-md-4">
-                        <Newsitem title="Sports" descriptionn="mydesc" imageurl="https://images.app.goo.gl/RjsNGfLeNhPyVqpi6"/>
-                    </div>
-
-                    <div className="col-md-4">
-                        <Newsitem title="Entertainment" imageurl=""/>
-                    </div>
-
-                    <div className="col-md-4">
-                        <Newsitem title="Business" imageurl=""/>
-                    </div>
+                {this.state.articles.map((element)=>{
+                     return <div className="col-md-4" key={element.url}>
+                     <Newsitem  title={element.title?element.title.slice(0,100):""}  imageurl={element.urlToImage} newsurl={element.url}/>
+                 </div>
+                    
+                })}
 
 
                 </div>
-                <br></br>
-                <br></br>
-
-                <div className="row">
-
-                    <div className="col-md-4">
-                        <Newsitem title="Technology" descriptionn="mydesc" imageurl="https://images.app.goo.gl/RjsNGfLeNhPyVqpi6"/>
-                    </div>
-
-                    <div className="col-md-4">
-                        <Newsitem title="Politics" imageurl=""/>
-                    </div>
-
-                    <div className="col-md-4">
-                        <Newsitem title="International" imageurl=""/>
-                    </div>
-
-
-                </div>
+                
 
             </div>
 
