bda3327f592e72006f1cdeca6595aec8cada015d
SoumitraPratap <101451346+SoumitraPratap@users.noreply.github.com>
2023-10-25 15:42:39 +0530

    Update news.js

--- src/MyComponents/news.js
+++ src/MyComponents/news.js
@@ -1,47 +1,18 @@
-import React, { Component } from 'react'
+import { React, Component, useState } from 'react'
 import Newsitem from './Newsitem'
 
-export class news extends Component {
-    
-    constructor(){
-        super();
-        this.state={
-            articles: [],
-            loading:false
-            
-        }
-    }
-
-    async componentDidMount(){
-        let url="https://newsapi.org/v2/everything?q=india&from=2023-09-24&sortBy=publishedAt&apiKey=aaee3d61e4684322813323f585d68de7";
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({articles:parsedData.articles});
-    }
-    render() {
-        return (
+export default function News(props) {
+    return (
             <div className="container my-3">
                 <h2>News Central - Because you deserve nothing but the truth</h2>
-                
                 <br></br>
                 <div className="row">
-
-                {this.state.articles.map((element)=>{
-                     return <div className="col-md-4" key={element.url}>
-                     <Newsitem  title={element.title?element.title.slice(0,100):""}  imageurl={element.urlToImage} newsurl={element.url}/>
-                 </div>
-                    
-                })}
-
-
+                { props.articles.map((element) => (
+                    <div className="col-md-4" key={element.url}>
+                        <Newsitem  title={element.title?element.title.slice(0,100):""}  imageurl={element.urlToImage} newsurl={element.url}/>
+                    </div>
+                ))}
                 </div>
-                
-
             </div>
-
-
-        )
-    }
+    )
 }
-
-export default news
