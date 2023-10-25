cf2df98f89490072daff2f8276896d38c6721519
SoumitraPratap <101451346+SoumitraPratap@users.noreply.github.com>
2023-10-25 15:41:11 +0530

    Update Newsitem.js

--- src/MyComponents/Newsitem.js
+++ src/MyComponents/Newsitem.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-    let {title , description , imageurl} = this.props;   // destructuring
+    let fallbackurl = "https://images.indianexpress.com/2023/10/express-view-36.jpg";
+    let {title , description , imageurl , newsurl} = this.props;   // destructuring
     return (
 
-      <div>
+      <div className="container my-3">
         <div className="card" style={{width: "18rem;"}}>
-            <img src={imageurl} className="card-img-top" alt="..."/>
+            <img src={ imageurl ? imageurl : fallbackurl } className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title} ...</h5>
                 <p className="card-text">{description}</p>
-                <a href="/newsdetail" className="btn btn-primary">Explore</a>
+                <a href={newsurl} className="btn btn-primary">Explore</a>
             </div>
             </div>
       </div>
