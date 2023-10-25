c705a491347ce2a327ac2b9fda0266231111b22f
SoumitraPratap <101451346+SoumitraPratap@users.noreply.github.com>
2023-10-25 15:40:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 import Header from "./MyComponents/header";
+import Footer from './MyComponents/footer';
 import News from './MyComponents/news';
 
 
 function App() {
+  let [articles, setArticles] = useState([]);
+
   return (
     <>
-    <Header title="News Central"/>
-    <News/>
-
+    <Header title="News Central" setArticles={setArticles}/>
+    <News articles={articles}/>
+    <Footer />
     </>
-
-    
-    
-    
   );
 }
 
